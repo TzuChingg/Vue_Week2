516236c30c4f80a52fe3f223cef8694de8256e73
TzuChingg <suger5769@gmail.com>
2024-01-08 01:43:42 +0800

    first commit

---
+++ login.js
@@ -0,0 +1,16 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+const app = {
+    data() {
+        return {
+           email: "",
+           password: ""
+        }
+    },
+    created() {
+        
+    },
+    mounted() {
+        
+    }
+}
+createApp(app).mount('#app')
